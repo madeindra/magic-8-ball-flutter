526361a18358e2f24a6ef80b5636fa389a95d5aa
Made Indra <made.indra@protonmail.com>
2021-02-18 19:30:22 +0700

    create stateless widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,29 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  runApp(MaterialApp(
+      home: Scaffold(
+    backgroundColor: Colors.blue,
+    appBar: AppBar(
+      title: Text('Magic 8 Ball'),
+      backgroundColor: Colors.blue,
+    ),
+    body: MagicBall(),
+  )));
+}
+
+class MagicBall extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child: FlatButton(
+          onPressed: () {
+            print('pressed');
+          },
+          child: Image.asset('images/ball3.png'),
+        ),
       ),
     );
+  }
+}
